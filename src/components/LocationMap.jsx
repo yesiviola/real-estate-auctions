d42979676808a9cdb58cf94eee4e9a2b77e5e39e
yesenia <aryesigon@gmail.com>
2024-07-27 19:51:03 -0300

    styles table

--- src/components/LocationMap.jsx
+++ src/components/LocationMap.jsx
@@ -19,6 +19,101 @@ const LocationMap = () =>   {
                 title="Property Location"
 
             ></iframe>
+
+
+            <div className={styles.adFeatures}>
+                <h2>Ad Features</h2>
+                <div className={styles.featureSection}>
+                    <h3>Exterior Features</h3>
+
+                    <ul>
+                        <li><input type="checkbox" checked /> Car Park</li>
+                        <li><input type="checkbox" checked  /> Parking Garage</li>
+                        <li><input type="checkbox" checked /> Water Tank</li>
+                        <li><input type="checkbox" checked /> Thermal Insulation</li>
+                    </ul>
+                </div>
+
+                <div className={styles.featureSection}>
+                    <h3>Interior Features</h3>
+                    <ul>
+                        <li><input type="checkbox" checked /> Balcony</li>
+                        <li><input  type="checkbox" checked /> Built-in kitchen</li>
+                        <li><input type="checkbox"  checked /> Blinds</li>
+
+                    </ul>
+                </div>
+
+                <div className={styles.featuresSection}>
+                    <h3>Location Features</h3>
+                    <ul>
+                        <li><input type="checkbox" checked />Local</li>
+                        <li><input type="checkbox" checked />Eastern Front</li>
+                        <li><input type="checkbox" checked />Mountain View</li>
+                    </ul>
+                </div>
+            </div>
+            <div className={styles.planningZone}>
+                <h2>Planning Zone</h2>
+                <div className={styles.zoneSection}>
+                    <h3>Residential (KA8)</h3>
+                    <table className={styles.table}>
+                        <tbody>
+                            <tr>
+                                <td>Description</td>
+                                <td>Areas With Dominant Use The Residence</td>
+                            </tr>
+                            <tr>
+                                <td>Coverage</td>
+                                <td>0.35</td>
+                            </tr>
+                            <tr>
+                                <td>Density</td>
+                                <td>0.7</td>
+                            </tr>
+                            <tr>
+                                <td>Height</td>
+                                <td>8.3 metres</td>
+                            </tr>
+                            <tr>
+                                <td>Allowed Stories</td>
+                                <td>2</td>
+                            </tr>
+                            <tr>
+                                <td>Development Desc.</td>
+                                <td>Special Policy Statement</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div className={styles.zoneSection}>
+                    <h3>Near By Planning Zones</h3>
+                    <table className={styles.table}>
+                        <tbody>
+                            <tr>
+                                <td>Commercial</td>
+                                <td>1 KM</td>
+                            </tr>
+                            <tr>
+                                <td>Tourist</td>
+                                <td>0.35 KM</td>
+                            </tr>
+                            <tr>
+                                <td>Industrial</td>
+                                <td>0.7 KM</td>
+                            </tr>
+                            <tr>
+                                <td>Agricultural</td>
+                                <td>8.3 KM</td>
+                            </tr>
+                            <tr>
+                                <td>Farm</td>
+                                <td>2 KM</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </section>
     )
 }
